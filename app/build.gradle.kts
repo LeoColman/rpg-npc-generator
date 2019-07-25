8f9d178d86ebf7b3338c7379596a0c299b073008
Kerooker <leonardo.colman98@gmail.com>
2019-07-25 18:11:27 -0300

    Sets up automated delivery and build
    Closes #6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
 import com.android.build.gradle.internal.dsl.DefaultConfig
 import com.github.triplet.gradle.play.PlayPublisherExtension
+import groovy.lang.Closure
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import java.util.*
 
@@ -25,6 +26,7 @@ android {
     setupSigningConfigs()
     setupBuildTypes()
     setupFlavors()
+    setupTests()
 }
 
 fun DefaultConfig.setupVersion() {
@@ -33,14 +35,16 @@ fun DefaultConfig.setupVersion() {
 }
 
 fun BaseAppModuleExtension.setupSigningConfigs() {
-    val localProps = Properties().apply { load(project.rootProject.file("local.properties").inputStream()) }
 
     signingConfigs {
         create("release") {
-            storeFile = file(localProps["key_file_location"]!!)
-            storePassword = localProps["keystore_password"].toString()
-            keyAlias = localProps["key_alias"].toString()
-            keyPassword = localProps["key_password"].toString()
+            try {
+                val localProps = Properties().apply { load(project.rootProject.file("local.properties").inputStream()) }
+                storeFile = file(localProps["key_file_location"]!!)
+                storePassword = localProps["keystore_password"].toString()
+                keyAlias = localProps["key_alias"].toString()
+                keyPassword = localProps["key_password"].toString()
+            } catch (_: Throwable) { }
         }
     }
 }
@@ -89,4 +93,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("androidx.core:core-ktx:1.0.2")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    // Testing
+    implementation("io.kotlintest:kotlintest-runner-junit5:3.4.0")
+}
+
+fun BaseAppModuleExtension.setupTests() {
+    testOptions {
+        unitTests.all(closureOf<Test> {
+            useJUnitPlatform()
+        } as Closure<Test>)
+    }
 }
