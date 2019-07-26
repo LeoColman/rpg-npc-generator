941062fbcd6aec2df4461a947a9c3f931a9c7ae2
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-07-25 21:45:49 -0300

    Sets up automated delivery and build (#7)
    Closes #6

---
+++ app/build.gradle.kts
@@ -0,0 +1,108 @@
+import com.android.build.gradle.internal.dsl.BaseAppModuleExtension
+import com.android.build.gradle.internal.dsl.DefaultConfig
+import com.github.triplet.gradle.play.PlayPublisherExtension
+import groovy.lang.Closure
+import org.jetbrains.kotlin.config.KotlinCompilerVersion
+import java.util.*
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    id("com.github.triplet.play") version "2.3.0"
+    id("net.thauvin.erik.gradle.semver") version "1.0.3-beta"
+}
+
+android {
+    compileSdkVersion(29)
+    buildToolsVersion("29.0.0")
+
+    defaultConfig {
+        minSdkVersion(21)
+        targetSdkVersion(29)
+
+        setupVersion()
+    }
+
+    setupSigningConfigs()
+    setupBuildTypes()
+    setupFlavors()
+    setupTests()
+}
+
+fun DefaultConfig.setupVersion() {
+    versionName = semver.semver
+    versionCode = semver.major * 10_000 + semver.minor * 100 + semver.patch
+}
+
+fun BaseAppModuleExtension.setupSigningConfigs() {
+
+    signingConfigs {
+        create("release") {
+            try {
+                val localProps = Properties().apply { load(project.rootProject.file("local.properties").inputStream()) }
+                storeFile = file(localProps["key_file_location"]!!)
+                storePassword = localProps["keystore_password"].toString()
+                keyAlias = localProps["key_alias"].toString()
+                keyPassword = localProps["key_password"].toString()
+            } catch (_: Throwable) { }
+        }
+    }
+}
+
+fun BaseAppModuleExtension.setupBuildTypes() {
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+
+        getByName("debug") {
+            applicationIdSuffix = ".debug"
+            resValue("string", "app_name", "RPG NPC Generator Debug")
+        }
+    }
+}
+
+fun BaseAppModuleExtension.setupFlavors() {
+    flavorDimensions("version")
+
+    productFlavors {
+        create("free") {
+            applicationId = "me.kerooker.rpgnpcgenerator"
+            resValue("string", "app_name", "RPG NPC Generator")
+        }
+        create("pro") {
+            applicationId = "me.kerooker.rpgnpcgeneratorpro"
+            resValue("string", "app_name", "RPG NPC Generator PRO")
+
+        }
+    }
+}
+
+configure<PlayPublisherExtension> {
+    track = "beta"
+    serviceAccountCredentials = file("../local/play-store-key.json")
+}
+
+dependencies {
+    // Kotlin
+    implementation(kotlin("stdlib-jdk8", KotlinCompilerVersion.VERSION))
+
+    // Android
+    implementation("androidx.appcompat:appcompat:1.0.2")
+    implementation("androidx.core:core-ktx:1.0.2")
+    implementation("androidx.constraintlayout:constraintlayout:1.1.3")
+
+    // Testing
+    implementation("io.kotlintest:kotlintest-runner-junit5:3.4.0")
+}
+
+fun BaseAppModuleExtension.setupTests() {
+    testOptions {
+        unitTests.all(closureOf<Test> {
+            useJUnitPlatform()
+        } as Closure<Test>)
+    }
+}
