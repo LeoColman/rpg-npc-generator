d34d46123b72303e16ab00f21c25a63d5136ca74
Leonardo Colman Lopes <leonardo.colman98@gmail.com>
2019-12-21 01:02:14 -0300

    Add import of Legacy NPCS (#28)
    Closes #27

--- app/src/main/java/me/kerooker/rpgnpcgenerator/RpgNpcGeneratorApplication.kt
+++ app/src/main/java/me/kerooker/rpgnpcgenerator/RpgNpcGeneratorApplication.kt
@@ -1,21 +1,26 @@
 package me.kerooker.rpgnpcgenerator
 
 import android.app.Application
+import me.kerooker.rpgnpcgenerator.legacy.repository.LegacyNpcImporter
 import me.kerooker.rpgnpcgenerator.legacy.repository.LegacyNpcRepository
 import me.kerooker.rpgnpcgenerator.repository.model.persistence.objectBoxModule
 import me.kerooker.rpgnpcgenerator.repository.model.random.npc.npcGeneratorsModule
 import me.kerooker.rpgnpcgenerator.repository.model.random.npc.randomNpcModule
 import me.kerooker.rpgnpcgenerator.viewmodel.viewModelsModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 class RpgNpcGeneratorApplication : Application() {
 
+    private val legacyNpcImporter by inject<LegacyNpcImporter>()
+        
     override fun onCreate() {
         super.onCreate()
 
         startKoinModules()
+        legacyNpcImporter.importAll()
     }
 
     private fun startKoinModules() {
@@ -29,4 +34,5 @@ class RpgNpcGeneratorApplication : Application() {
 
 val mainModule = module {
     single { LegacyNpcRepository() }
+    single { LegacyNpcImporter(get(), get(), get()) }
 }
