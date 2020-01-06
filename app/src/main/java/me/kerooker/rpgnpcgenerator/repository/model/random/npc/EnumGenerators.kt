de399dce0c99953b7d88c72d12f203312a4ae75e
Kerooker <leonardo.colman98@gmail.com>
2020-01-06 19:30:33 -0300

    Fix Halfling subrace
    Halfling had their subraces as copies of Gnome subraces. This commit fixes that and lets Halflings have their subraces correctly.
    
    Fixes #33

--- app/src/main/java/me/kerooker/rpgnpcgenerator/repository/model/random/npc/EnumGenerators.kt
+++ app/src/main/java/me/kerooker/rpgnpcgenerator/repository/model/random/npc/EnumGenerators.kt
@@ -1,4 +1,5 @@
 @file:Suppress("MagicNumber")
+
 package me.kerooker.rpgnpcgenerator.repository.model.random.npc
 
 import androidx.annotation.StringRes
@@ -19,14 +20,14 @@ enum class Age(
     override val distribution: Double
 ) : RandomDistributed,
     NamedResource {
-
+    
     Child(R.string.age_child, 5.0),
     Teenager(R.string.age_teenager, 10.0),
     YoungAdult(R.string.age_young_adult, 35.0),
     Adult(R.string.age_adult, 35.0),
     Old(R.string.age_old, 10.0),
     VeryOld(R.string.age_very_old, 5.0);
-
+    
     companion object {
         fun distributedRandom() = values().toList().distributedRandom()
         fun random() = values().random()
@@ -38,9 +39,9 @@ enum class Alignment(
     override val distribution: Double
 ) : RandomDistributed,
     NamedResource {
-
+    
     LawfulGood(R.string.alignment_lawful_good, 2.0),
-    LawfulNeutral(R.string.alignment_lawful_neutral,10.0),
+    LawfulNeutral(R.string.alignment_lawful_neutral, 10.0),
     LawfulEvil(R.string.alignment_lawful_evil, 2.0),
     NeutralGood(R.string.alignment_neutral_good, 10.0),
     Neutral(R.string.alignment_neutral, 52.0),
@@ -48,12 +49,12 @@ enum class Alignment(
     ChaoticGood(R.string.alignment_chaotic_good, 2.0),
     ChaoticNeutral(R.string.alignment_chaotic_neutral, 10.0),
     ChaoticEvil(R.string.alignment_chaotic_evil, 2.0);
-
+    
     companion object {
         fun distributedRandom() = values().toList().distributedRandom()
         fun random() = values().random()
     }
-
+    
 }
 
 enum class Gender(
@@ -61,10 +62,10 @@ enum class Gender(
     override val distribution: Double
 ) : RandomDistributed,
     NamedResource {
-
+    
     Male(R.string.gender_male, 50.0),
     Female(R.string.gender_female, 50.0);
-
+    
     companion object {
         fun distributedRandom() = values().toList().distributedRandom()
         fun random() = values().random()
@@ -72,7 +73,7 @@ enum class Gender(
 }
 
 interface Language : NamedResource {
-
+    
     companion object {
         fun values(): List<Language> = CommonLanguage.values().toList() + ExoticLanguage.values().toList()
         fun random() =
@@ -91,7 +92,7 @@ enum class CommonLanguage(
     Goblin(R.string.language_goblin),
     Halfling(R.string.language_halfling),
     Orc(R.string.language_orc);
-
+    
     companion object {
         fun random(excluding: List<Language>) = values().filter { it !in excluding }.random()
     }
@@ -105,7 +106,7 @@ enum class ExoticLanguage(
     Infernal(R.string.language_infernal),
     Druidic(R.string.language_druidic),
     Draconic(R.string.language_draconic);
-
+    
     companion object {
         fun random(excluding: List<Language>) = values().filter { it !in excluding }.random()
     }
@@ -117,46 +118,30 @@ enum class Race(
     val racialLanguage: Language?
 ) : RandomDistributed,
     NamedResource {
-
-    HillDwarf(R.string.race_hill_dwarf, 8.75,
-        CommonLanguage.Dwarvish
-    ),
-    MountainDwarf(R.string.race_mountain_dwarf, 8.75,
-        CommonLanguage.Dwarvish
-    ),
-
+    
+    HillDwarf(R.string.race_hill_dwarf, 8.75, CommonLanguage.Dwarvish),
+    MountainDwarf(R.string.race_mountain_dwarf, 8.75, CommonLanguage.Dwarvish),
+    
     HighElf(R.string.race_high_elf, 5.84, CommonLanguage.Elvish),
     WoodElf(R.string.race_wood_elf, 5.83, CommonLanguage.Elvish),
     Drow(R.string.race_drow, 5.83, CommonLanguage.Elvish),
-
-    ForestHalfling(R.string.race_forest_halfling, 8.75,
-        CommonLanguage.Halfling
-    ),
-    RockHalfling(R.string.race_rock_halfling, 8.75,
-        CommonLanguage.Halfling
-    ),
-
+    
+    StoutHalfling(R.string.race_stout_halfling, 8.75, CommonLanguage.Halfling),
+    LightfootHalfling(R.string.race_lightfoot_halfling, 8.75, CommonLanguage.Halfling),
+    
     Human(R.string.race_human, 17.5, null),
-
-    Dragonborn(R.string.race_dragonborn, 6.0,
-        ExoticLanguage.Draconic
-    ),
-
-    ForestGnome(R.string.race_forest_gnome, 3.0,
-        CommonLanguage.Gnomish
-    ),
-    RockGnome(R.string.race_rock_gnome, 3.0,
-        CommonLanguage.Gnomish
-    ),
-
+    
+    Dragonborn(R.string.race_dragonborn, 6.0, ExoticLanguage.Draconic),
+    
+    ForestGnome(R.string.race_forest_gnome, 3.0, CommonLanguage.Gnomish),
+    RockGnome(R.string.race_rock_gnome, 3.0, CommonLanguage.Gnomish),
+    
     HalfElf(R.string.race_half_elf, 6.0, CommonLanguage.Elvish),
-
+    
     HalfOrc(R.string.race_half_orc, 6.0, CommonLanguage.Orc),
-
-    Tiefling(R.string.race_tiefling, 6.0,
-        ExoticLanguage.Infernal
-    );
-
+    
+    Tiefling(R.string.race_tiefling, 6.0, ExoticLanguage.Infernal);
+    
     companion object {
         fun distributedRandom() = values().toList().distributedRandom()
         fun random() = values().random()
@@ -168,40 +153,40 @@ enum class Sexuality(
     override val distribution: Double
 ) : RandomDistributed,
     NamedResource {
-
+    
     Homosexual(R.string.sexuality_homosexual, 2.0),
     Bisexual(R.string.sexuality_bisexual, 2.0),
     Asexual(R.string.sexuality_asexual, 1.0),
     Heterosexual(R.string.sexuality_heterosexual, 95.0);
-
+    
     companion object {
         fun distributedRandom() = values().toList().distributedRandom()
         fun random() = values().random()
     }
-
 }
 
 
 // Code inspired from https://github.com/thomasnield/kotlin-statistics/blob/master/src/main/kotlin/org/nield/kotlinstatistics/Random.kt#L111
-fun <T: RandomDistributed> List<T>.distributedRandom(): T {
+fun <T : RandomDistributed> List<T>.distributedRandom(): T {
     val probabilities = associateWith { it.distribution }
-
+    
     val sum = probabilities.values.sum()
-
+    
     val rangedDistribution = probabilities.run {
-
+        
         var binStart = 0.0
-
+        
         asSequence().sortedBy { it.value }
-            .map { it.key to OpenDoubleRange(
-                binStart,
-                it.value + binStart
-            )
+            .map {
+                it.key to OpenDoubleRange(
+                    binStart,
+                    it.value + binStart
+                )
             }
             .onEach { binStart = it.second.endExclusive }
             .toMap()
     }
-
+    
     return Random.nextDouble(0.0, sum).let {
         rangedDistribution.asIterable().first { rng -> it in rng.value }.key
     }
@@ -209,7 +194,6 @@ fun <T: RandomDistributed> List<T>.distributedRandom(): T {
 
 private data class OpenDoubleRange(val start: Double, val endExclusive: Double) {
     operator fun contains(it: Double): Boolean {
-        return it >=start && it < endExclusive
+        return it >= start && it < endExclusive
     }
-
 }
